db0911d7274bff9828e61b9561e0dff84b7cd0a1
klkirstyleeskl <k.lees@btinternet.com>
2018-11-26 16:23:58 +0000

    passes all of section A

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,30 +2,43 @@ require('minitest/autorun')
 require('minitest/rg')
 require_relative('../student')
 
-class TestStudentInformation < MiniTest::Test
+class TestStudent < MiniTest::Test
 
 def test_student_name
-  student_information = StudentInformation.new('Bob', 'E46')
-  assert_equal('Bob', student_information.name)
+  student = Student.new('Bob', 'E46')
+  assert_equal('Bob', student.name)
 end
 
 def test_student_cohort
-    student_information = StudentInformation.new('Bob', 'E46')
-    assert_equal('E46', student_information.cohort)
+    student = Student.new('Bob', 'E46')
+    assert_equal('E46', student.cohort)
 end
 
 
 def test_set_student_name
-    student_information = StudentInformation.new('Bob', 'E46')
-    student_information.name='Jenny'
-    assert_equal('Jenny', student_information.name)
+    student = Student.new('Bob', 'E46')
+    student.name='Jenny'
+    assert_equal('Jenny', student.name)
 end
 
 def test_set_student_cohort
-    student_information = StudentInformation.new('Bob', 'E46')
-    student_information.cohort='E26'
-    assert_equal('E26', student_information.cohort)
+    student = Student.new('Bob', 'E46')
+    student.cohort='E26'
+    assert_equal('E26', student.cohort)
 end
+
+def test_student_talk
+    student = Student.new('Bob', 'E46')
+    result = student.i_can_talk()
+    assert_equal( 'Bob I can talk', result)
+end
+
+def test_say_favourite_language
+    student = Student.new('Bob', 'E46')
+    result = student.say_favourite_language('Ruby')
+    assert_equal('I love Ruby', result)
+end
+
 
 
 
--- student.rb
+++ student.rb
@@ -1,12 +1,19 @@
-class StudentInformation
-attr_reader :name, :cohort
-attr_writer :name, :cohort
+class Student
+  attr_reader :name, :cohort
+  attr_writer :name, :cohort
 
-def initialize(name, cohort)
-  @name = name
-  @cohort = cohort
+  def initialize(name, cohort)
+    @name = name
+    @cohort = cohort
+  end
+
+  def i_can_talk
+    return @name + ' I can talk'
+  end
+
+  def say_favourite_language(language)
+    return "I love #{language}"
+  end
+
 end
-
-
-
-end
+ 
